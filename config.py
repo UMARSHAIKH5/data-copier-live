b926c3105ddd65dc4cb6d3142ceb6e89a19fe290
UMAR SHAIKH <umars3015@gmail.com>
2024-10-02 15:36:03 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+DB_Details={
+    'dev': {
+        'SOURCE_DB': {
+            'DB_TYPE': 'mysql',
+            'DB_HOST': '13.127.98.80',
+            'DB_NAME': 'retail_db',
+            'DB_USER': os.environ.get('TARGET_DB_USER'),
+            'DB_PASS':os.environ.get('SOURCE_DB_PASS')
+        },
+        'TARGET_DB': {
+            'DB_TYPE': 'postgres',
+            'DB_HOST': '13.127.98.80',
+            'DB_NAME': 'retail_db',
+            'DB_USER': os.environ.get('TARGET_DB_USER'),
+            'DB_PASS': os.environ.get('TARGET_DB_PASS')
+        }
+    }
+}
